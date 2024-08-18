353b71359f210e3718a78edbf690bd93ab4f5cce
Misha Panchenko <panch.misha@gmail.com>
2024-08-18 21:20:07 +0200

    add event loop

--- orgmate/main.py
+++ orgmate/main.py
@@ -1,18 +1,22 @@
 from argparse import ArgumentParser
+from cmd import Cmd
+from pathlib import Path
 
 import logging
+import os
 
 
 logger = logging.getLogger(__name__)
 
 
-class OrgMate:
-    def run(self):
-        logger.debug('Hello, world!')
+class OrgMate(Cmd):
+    def do_EOF(self, arg):
+        return True
 
 
 def parse_args():
     parser = ArgumentParser()
+    parser.add_argument('-d', '--dir', default='~/.orgmate')
     parser.add_argument('-v', '--verbose', action='store_true')
     return parser.parse_args()
 
@@ -20,8 +24,10 @@ def parse_args():
 def main():
     args = parse_args()
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
-    app = OrgMate()
-    app.run()
+    dir = Path(args.dir).expanduser()
+    dir.mkdir(parents=True, exist_ok=True)
+    os.chdir(dir)
+    OrgMate().cmdloop()
 
 
 if __name__ == '__main__':
