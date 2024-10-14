709ac6d0f996b17f3bbbbe52d5e1bf9bc5475806
Misha Panchenko <panch.misha@gmail.com>
2024-10-14 12:20:50 +0200

    read cwd from env variable

--- orgmate/main.py
+++ orgmate/main.py
@@ -1,14 +1,21 @@
 from argparse import ArgumentParser
 from pathlib import Path
 
+import logging
 import os
+
 from orgmate.cli import CLI
+
+
+logger = logging.getLogger(__name__)
 
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('-d', '--dir', default='~/.orgmate')
+    default_dir = os.environ.get('ORGMATE_DIR', '~/.orgmate')
+    parser.add_argument('-d', '--dir', default=default_dir)
     parser.add_argument('-c', '--clear-state', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
     return parser.parse_args()
 
 
@@ -16,7 +23,9 @@ def main():
     args = parse_args()
     dir = Path(args.dir).expanduser()
     dir.mkdir(parents=True, exist_ok=True)
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
     os.chdir(dir)
+    logger.debug('Current working directory is %s', dir)
     CLI(args.clear_state).cmdloop()
 
 
